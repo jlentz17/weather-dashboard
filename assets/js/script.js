670900493b6abc81e5e588548f167da42130a0c7
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-06-07 23:59:30 -0700

    obtained form input data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
 // define variable for apiKey and endpoint so its easier
-var apiKey = "3870f0a27cb3db5d0e47646b3e356296";
-var endPoint= `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`
+var api_key = "3870f0a27cb3db5d0e47646b3e356296";
+var endPoint= `https://api.openweathermap.org/data/2.5/weather?q=${formValue}&appid=${api_key}`
+
+var formValue;
 
 // fetch Weather Data
+
 function getWeather() {
 var currentCityWeatherName;
 fetch(endPoint)
 .then((response)=> response.json()) 
 .then(function(weather){
-currentCityWeatherName=weather.city.name;
+console.log(weather)
 console.log(currentCityWeatherName);
 addWeatherData(currentCityWeatherName)
 });
+
+var formInput = document.querySelector("#user-form");
+    formInput.addEventListener("submit", function(event){
+        event.preventDefault();
+        formValue = event.target[0].value;
+        console.log(formValue);
+        getWeather();
+    })
+    
+}
+
 // var currentDate;
 // fetch(endPoint)
 // .then((response)=> response.json())
@@ -26,13 +40,13 @@ addWeatherData(currentCityWeatherName)
 // // var weatherEl = document.querySelector(".weatherEl");
 // // weatherEl.innerText()
 // });
-}
+
 function addWeatherData(currentCityWeatherName){
 var weatherEl = document.querySelector(".weatherEl");
 weatherEl.textContent = currentCityWeatherName;
 // var weatherDateEl = document.querySelector(".weatherDateEl");
 // weatherDateEl.textContent = currentDate
 }
-getWeather();
-console.log(document);
+// getWeather();
+// console.log(document);
 
